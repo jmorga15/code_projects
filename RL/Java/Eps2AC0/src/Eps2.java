24b4e117043aba39c5cdeb6c20532c3f4031890f
jmorga15 <108193756+jmorga15@users.noreply.github.com>
2023-12-10 09:12:08 -0500

    Java RL Eps2AC0

---
+++ RL/Java/Eps2AC0/src/Eps2.java
@@ -0,0 +1,12 @@
+public class Eps2{
+    // Possible states
+    private double[] state;
+
+    // Constructor initializes the environment to the t1 state
+    public Eps2() {
+        // Initial state at t1: visual cue shown
+        state = new double[] {
+
+        }
+    }
+}
